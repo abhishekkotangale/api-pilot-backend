03979238e3b599266f1d8bdb99f8349d0d49c062
abhishek <abhishekkotangale10@gmail.com>
2026-01-04 15:34:57 +0530

    [added logs]

--- config/db.js
+++ config/db.js
@@ -2,9 +2,11 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
+    console.log("mongo url", process.env.MONGO_URL);
     await mongoose.connect(process.env.MONGO_URL);
     console.log("MongoDB Connected");
   } catch (err) {
+    console.log("error occured while connecting db")
     console.error(err);
   }
 };
